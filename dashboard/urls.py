3fb908d5ab4a5d7d5e07893da51b70bea8e98157
i.pavlov <i.pavlov@2gis.ru>
2014-11-05 09:12:50 +0700

    add sessions table with pagination

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from dashboard import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    # url(r'^(?P<session_name>\d+)/$', views.exact_session, name='exact_session'),
+    url(r'^$', views.dashboard, name='dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,17 +5,17 @@ from dashboard.models import Sessions
 from api.views import get_platforms, get_queue, get_sessions
 
 
-def index(request):
+def dashboard(request):
     try:
         session = request.GET["q"]
     except:
         session = ''
 
     if not session:
-        sessions = Sessions.objects.order_by('time')[:20].reverse()
+        sessions = Sessions.objects.order_by('-time')
     else:
         try:
-            sessions = Sessions.objects.get(name=session)
+            sessions = Sessions.objects.filter(name=session)
         except ObjectDoesNotExist:
             sessions = None
 
