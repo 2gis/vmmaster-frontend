f236ea84b0ee86b97f27c2b2dd574d4df190e101
Nikolay Ustinov <n.ustinov@2gis.ru>
2017-11-02 17:18:45 +0700

    add new status for watchserver

--- watcher/sessions.py
+++ watcher/sessions.py
@@ -1,5 +1,6 @@
 import json
 import time
+import logging
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'vmmaster_frontend.settings'
@@ -10,6 +11,10 @@ from dashboard.models import Session
 from threading import Thread
 from django.core import serializers
 from django.utils import timezone
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 class WatchSessionsThread(Thread):
@@ -28,6 +33,7 @@ class WatchSessionsThread(Thread):
         active_session_ids = []
         running_session_ids = []
         waiting_session_ids = []
+        preparing_session_ids = []
 
         print 'Start time: %s' % date
 
@@ -35,17 +41,19 @@ class WatchSessionsThread(Thread):
             for session in sessions_on_page:
                 if session['status'] == 'waiting':
                     waiting_session_ids.append(session['id'])
-                elif session['status'] == 'running':
+                if session['status'] == 'running':
                     running_session_ids.append(session['id'])
+                if session['status'] == 'preparing':
+                    preparing_session_ids.append(session['id'])
 
-            active_session_ids = waiting_session_ids + running_session_ids
+            active_session_ids = waiting_session_ids + running_session_ids + preparing_session_ids
 
         while self.running:
             time.sleep(3)
             db_sessions = self.new_sessions_in_db(date, query)
 
             # watching count sessions in db
-            # check this if you changed django version to 1.8
+            # TODO: check this if you changed django version to 1.8
             if db_sessions.count() > 0:
                 session_ids = [session.id for session in db_sessions]
 
@@ -53,11 +61,11 @@ class WatchSessionsThread(Thread):
                     session_ids = list(set(self.sent_sessions) ^ set(session_ids))
 
                 if session_ids:
-                    print 'New sessions for send: ', db_sessions.count()
+                    log.info('New sessions for send: {}'.format(db_sessions.count()))
 
                     serialized_obj = serializers.serialize('json', db_sessions.filter(id__in=session_ids))
                     diff = json.dumps(serialized_obj)
-                    diff = 'new#vmmaster_wsmessage#%s' % diff
+                    diff = 'new#vmmaster_wsmessage#{}'.format(diff)
 
                     self.ws.sendMessage(diff)
                     self.sent_sessions.extend(session_ids)
@@ -67,6 +75,7 @@ class WatchSessionsThread(Thread):
             if active_session_ids:
                 waiting_db_session_ids = []
                 running_db_session_ids = []
+                preparing_db_session_ids = []
 
                 print 'Active sessions on page: ', len(active_session_ids)
                 active_db_sessions = self.active_sessions_in_db(query)
@@ -74,22 +83,27 @@ class WatchSessionsThread(Thread):
                 for db_session in active_db_sessions:
                     if db_session.status == 'waiting':
                         waiting_db_session_ids.append(db_session.id)
-                    elif db_session.status == 'running':
+                    if db_session.status == 'running':
                         running_db_session_ids.append(db_session.id)
+                    if db_session.status == 'preparing':
+                        preparing_db_session_ids.append(db_session.id)
 
                 waiting_session_for_send = list(set(waiting_session_ids) - set(waiting_db_session_ids))
                 running_session_for_send = list(set(running_session_ids) - set(running_db_session_ids))
-                active_session_for_send = waiting_session_for_send + running_session_for_send
+                preparing_session_for_send = list(set(preparing_session_ids) - set(preparing_db_session_ids))
+                active_session_for_send = waiting_session_for_send + running_session_for_send + preparing_session_for_send
 
-                print 'Active sessions in db: ', active_db_sessions.count()
-                print 'Waiting sessions %s and running sessions %s ' % (waiting_db_session_ids, running_db_session_ids)
+                log.info('Active sessions in db: {}'.format(active_db_sessions.count()))
+                log.info('Waiting sessions {} and running sessions {} and prepating sessions {}'.format(
+                    waiting_db_session_ids, running_db_session_ids, preparing_session_for_send)
+                )
 
                 if active_session_for_send:
-                    print 'Active sessions for send: ', active_session_for_send
+                    log.info('Active sessions for send: {}'.format(active_session_for_send))
 
                     serialized_obj = serializers.serialize('json', Session.objects.filter(id__in=active_session_for_send))
                     diff = json.dumps(serialized_obj)
-                    diff = 'update#vmmaster_wsmessage#%s' % diff
+                    diff = 'update#vmmaster_wsmessage#{}'.format(diff)
 
                     self.ws.sendMessage(diff)
                     self.sent_sessions.extend(active_session_for_send)
@@ -98,11 +112,11 @@ class WatchSessionsThread(Thread):
     def active_sessions_in_db(self, query):
         if self.ws.user.is_superuser:
             active_db_sessions = Session.objects\
-                .filter(status__in=('waiting', 'running'))\
+                .filter(status__in=('waiting', 'running', 'preparing'))\
                 .filter(name__icontains=query)
         else:
             active_db_sessions = Session.objects\
-                .filter(status__in=('waiting', 'running'))\
+                .filter(status__in=('waiting', 'running', 'preparing'))\
                 .filter(user=self.ws.user)\
                 .filter(name__icontains=query)
 
